901e847e32cf6a4dad1f321ecc3315a0e038be30
XXX Ahana <ahanasi@seas.upenn.edu>
2021-06-05 22:05:37 -0700

    Add win condition

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 const gameBoard = (() => {
   const boardArr = new Array(9);
+  const winConditions = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
   const getBoard = () => boardArr;
   const setBoard = (index, marker) => {
     boardArr[index] = marker;
   };
 
   const resetBoard = () => {
-    boardArr.splice(0, boardArr.length)
-  }
+    boardArr.splice(0, boardArr.length);
+  };
 
-  return { getBoard, setBoard, resetBoard };
+  const isWin = (index) => winConditions.some((winCondition) => {
+      if (
+        winCondition.includes(index) &&
+        boardArr[winCondition[0] - 1] === boardArr[winCondition[1] - 1] &&
+        boardArr[winCondition[0] - 1] === boardArr[winCondition[2] - 1] &&
+        boardArr[winCondition[0] - 1] != null
+      ) {
+        return true;
+      }
+      return false;
+    });
+
+  return { getBoard, setBoard, resetBoard, isWin };
 })();
 
 const displayController = ((doc) => {
@@ -24,21 +46,21 @@ const displayController = ((doc) => {
     }
   };
 
-  const changeMarker = (tile, marker) => {
+  const placeMarker = (tile, marker) => {
     const chosenTile = tile;
     chosenTile.textContent = marker;
   };
 
   const reset = () => {
     if (!!doc && 'querySelector' in doc) {
-      tiles.forEach((tile ) => {
+      tiles.forEach((tile) => {
         const chosenTile = tile;
-        chosenTile.textContent = "";
+        chosenTile.textContent = '';
       });
     }
-  }
+  };
 
-  return { displayToDOM, changeMarker, reset };
+  return { displayToDOM, placeMarker, reset };
 })(document);
 
 const playerFactory = (name, marker) => {
@@ -65,17 +87,21 @@ const game = (doc) => {
     gameBoard.resetBoard();
     displayController.reset();
     game(document);
-  }
-  
+  };
+
   const round = (element) => {
     const chosenTile = element;
     const markerPosition = chosenTile.dataset.index;
     displayController.displayToDOM();
 
-    if (chosenTile.textContent === "") {
-      displayController.changeMarker(chosenTile, currentPlayer.getMarker());
+    if (chosenTile.textContent === '') {
+      displayController.placeMarker(chosenTile, currentPlayer.getMarker());
       gameBoard.setBoard(markerPosition - 1, currentPlayer.getMarker());
-      switchPlayer();
+      if (gameBoard.isWin(parseInt(chosenTile.dataset.index, 10))) {
+        alert(`${currentPlayer.getName()} has won!`);
+      } else {
+        switchPlayer();
+      }
     }
   };
 
@@ -93,4 +119,3 @@ const game = (doc) => {
 };
 
 game(document);
-
